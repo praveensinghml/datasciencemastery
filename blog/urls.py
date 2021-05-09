271d2c91a2b0b004278b0b08cab7942c46b8a7fa
LEGAL\singhp7 <praveen.singh@lexisnexis.com>
2021-05-09 14:21:02 +0530

    first

---
+++ blog/urls.py
@@ -0,0 +1,62 @@
+from django.conf import settings
+from django.conf.urls.static import static
+from django.contrib import admin
+from django.urls import path, include
+from django.views.generic.base import TemplateView # new
+
+from posts.views import (
+    PracticeView,
+    VoteView,
+    IndexView,
+    SearchView,
+    blog_category,
+    blog_tags,
+    PostListView,
+    PostDetailView,
+    PostCreateView,
+    PostUpdateView,
+    PostDeleteView,
+    ContactView,
+    upload,
+    user_dashboard  
+    )
+from marketing.views import email_list_signup
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    # path('', index),
+    path('', IndexView.as_view(), name='home'),
+    # path('blog/', post_list, name='post-list'),
+    path('blog/', PostListView.as_view(), name='post-list'),
+    path('search/', SearchView.as_view(), name='search'),
+    path('email-signup/', email_list_signup, name='email-list-signup'),
+    # path('create/', post_create, name='post-create'),
+    path('create/', PostCreateView.as_view(), name='post-create'),
+    path('upload/', upload, name='upload_img'),
+    # path('post/<id>/', post_detail, name='post-detail'),
+    path('post/<str:slug>-<int:pk>/', PostDetailView.as_view(), name='post-detail'),
+    # path('post/<id>/update/', post_update, name='post-update'),
+    path('post/<int:pk>/update/', PostUpdateView.as_view(), name='post-update'),
+    # path('post/<id>/delete/', post_delete, name='post-delete'),
+    path('post/<int:pk>/delete/', PostDeleteView.as_view(), name='post-delete'),
+    path('contact/', ContactView, name='contact'),
+    path('vote/', VoteView, name='vote_post'),
+    path('practice/', PracticeView, name='practice'),
+    
+    path('searchbycat/<category>/', blog_category, name='searchbycat'),
+    path('searchbytag/<tags>/', blog_tags, name='searchbytag'),
+    
+    path('tinymce/', include('tinymce.urls')),
+    path('accounts/', include('allauth.urls')),
+    path('accounts/profile/',
+         user_dashboard.as_view(),
+         name='profile')
+
+]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL,
+                          document_root=settings.STATIC_ROOT)
+    urlpatterns += static(settings.MEDIA_URL,
+                          document_root=settings.MEDIA_ROOT)
+                           
---
+++ posts/views.py
@@ -0,0 +1,369 @@
+from django.http import JsonResponse
+from django.db.models import Count, Q
+from django.contrib import messages
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+from django.shortcuts import render, get_object_or_404, redirect, reverse
+from django.views.generic import View, ListView, DetailView, CreateView, UpdateView, DeleteView
+
+from .forms import CommentForm, PostForm
+from .models import Post, Author, PostView,Comment
+from marketing.forms import EmailSignupForm
+from marketing.models import Signup
+from django.urls import reverse_lazy, reverse
+from django.http import HttpResponseRedirect
+from django.template.defaultfilters import slugify
+from django.core.mail import send_mail
+from django.conf import settings
+
+from django.views.decorators.csrf import csrf_exempt
+from django.contrib.auth.decorators import login_required
+from PIL import Image
+from django.http import HttpResponse
+import uuid
+
+
+form = EmailSignupForm()
+
+
+def get_author(user):
+    qs = Author.objects.filter(user=user)
+    if qs.exists():
+        return qs[0]
+    return None
+
+def get_category_count():
+    queryset = Post \
+        .objects \
+        .values('categories__title') \
+        .annotate(Count('categories__title'))
+    return queryset
+
+def get_tags_count():
+    queryset = Post \
+        .objects \
+        .values('tags__title') \
+        .annotate(Count('tags__title'))
+    return queryset
+
+class SearchView(View):
+    def get(self, request, *args, **kwargs):
+        queryset = Post.objects.all()
+        query = request.GET.get('q')
+        if query:
+            queryset = queryset.filter(
+                Q(title__icontains=query) |
+                Q(overview__icontains=query)
+            ).distinct()
+        context = {
+            'queryset': queryset
+        }
+        return render(request, 'search_results.html', context)
+
+
+def blog_category(request, category):
+    category_count = get_category_count()
+    most_recent = Post.objects.order_by('-timestamp')[:3]
+    post_list = Post.objects.filter(
+        categories__title=category
+    )
+    tags = get_tags_count()
+    paginator = Paginator(post_list, 4)
+    page_request_var = 'page'
+    page = request.GET.get(page_request_var)
+    try:
+        paginated_queryset = paginator.page(page)
+    except PageNotAnInteger:
+        paginated_queryset = paginator.page(1)
+    except EmptyPage:
+        paginated_queryset = paginator.page(paginator.num_pages)
+
+    context = {
+        'queryset': paginated_queryset,
+        'most_recent': most_recent,
+        'page_request_var': page_request_var,
+        'category_count': category_count,
+        'tags': tags,
+        'form': form
+    }
+    return render(request, 'blog.html', context)
+
+
+def ContactView(request):
+    message_email = ""
+    if request.method == 'POST':
+        message_email = request.POST['email'] 
+        message_subject  = request.POST['subject'] 
+        message  = request.POST['message']
+        #send a email
+        send_mail(message_subject, message, settings.EMAIL_HOST_USER, [message_email])
+
+    return render(request, "contacts.html", {'msg': message_email})
+
+
+def blog_tags(request, tags):
+    category_count = get_category_count()
+    most_recent = Post.objects.order_by('-timestamp')[:3]
+    post_list = Post.objects.filter(
+        tags__title=tags
+    )
+    tags = get_tags_count()
+    paginator = Paginator(post_list, 4)
+    page_request_var = 'page'
+    page = request.GET.get(page_request_var)
+    try:
+        paginated_queryset = paginator.page(page)
+    except PageNotAnInteger:
+        paginated_queryset = paginator.page(1)
+    except EmptyPage:
+        paginated_queryset = paginator.page(paginator.num_pages)
+
+    context = {
+        'queryset': paginated_queryset,
+        'most_recent': most_recent,
+        'page_request_var': page_request_var,
+        'category_count': category_count,
+        'tags': tags,
+        'form': form
+    }
+    return render(request, 'blog.html', context)
+
+
+
+
+
+
+class IndexView(View):
+    form = EmailSignupForm()
+
+    def get(self, request, *args, **kwargs):
+        category_count = get_category_count()
+        most_recent = Post.objects.filter(featured=True).order_by('-timestamp')[:3]
+        post_list = Post.objects.filter(featured=True).all()
+        tags = get_tags_count()
+        paginator = Paginator(post_list, 2)
+        page_request_var = 'page'
+        page = request.GET.get(page_request_var)
+        try:
+            paginated_queryset = paginator.page(page)
+        except PageNotAnInteger:
+            paginated_queryset = paginator.page(1)
+        except EmptyPage:
+            paginated_queryset = paginator.page(paginator.num_pages)
+        context = {
+            'object_list': paginated_queryset,
+            'most_recent': most_recent, 
+            'page_request_var': page_request_var,
+            'category_count': category_count,
+            'tags': tags,
+            'form': self.form
+        }
+        return render(request, 'index.html', context)
+
+    def post(self, request, *args, **kwargs):
+        email = request.POST.get("email")
+        new_signup = Signup()
+        new_signup.email = email
+        new_signup.save()
+        messages.info(request, "Successfully subscribed")
+        return redirect("home")
+
+
+
+
+
+class PostListView(ListView):
+    form = EmailSignupForm()
+    model = Post
+    template_name = 'blog.html'
+    context_object_name = 'queryset'
+    paginate_by =2
+
+    def get_context_data(self, **kwargs):
+        category_count = get_category_count()
+        most_recent = Post.objects.filter(featured=True).order_by('-timestamp')[:3]
+        tags = get_tags_count()
+        context = super().get_context_data(**kwargs)
+        context['most_related'] = most_recent
+        context['page_request_var'] = "page"
+        context['category_count'] = category_count
+        context['tags'] = tags
+        context['form'] = self.form
+        return context
+
+
+
+class PostDetailView(DetailView):
+    model = Post
+    template_name = 'post.html'
+    context_object_name = 'post'
+    form = CommentForm()
+
+    def get_object(self):
+        obj = super().get_object()
+        if self.request.user.is_authenticated:
+            PostView.objects.get_or_create(
+                user=self.request.user,
+                post=obj
+            )
+        return obj
+
+    def get_context_data(self, **kwargs):
+        category_count = get_category_count()
+        post = get_object_or_404(Post, id= self.kwargs['pk'])
+        rhs_tags = get_tags_count()
+        most_relative = Post.objects.order_by('-timestamp')[:3]
+        liked = False
+        if post.votes.filter(id = self.request.user.id).exists():
+            liked = True
+        current_author = get_author(self.request.user)
+        userpostcount = post_count(current_author)
+        author_votecount = 0
+        for n in Post.objects.filter(author=current_author):
+            author_votecount += n.votes.count()
+
+        context = super().get_context_data(**kwargs)
+        context['most_relative'] = most_relative
+        context['page_request_var'] = "page"
+        context['category_count'] = category_count
+        context['liked'] = liked
+        context['vote_count'] = post.vote_count
+        context['tags'] = post.get_tags[0],
+        context['rhs_tags'] = rhs_tags[0]
+        context['author_details'] = post.get_authordetails
+        context['authorpostcount'] = userpostcount
+        context['authorvotecount'] = author_votecount
+        
+        
+        context['form'] = self.form
+        return context
+
+    def post(self, request, *args, **kwargs):
+        form = CommentForm(request.POST)
+        if form.is_valid():
+            post = self.get_object()
+            form.instance.user = request.user
+            form.instance.post = post
+            form.save()
+            return redirect(reverse("post-detail", kwargs={
+                'pk': post.pk   
+            }))
+
+
+class PostCreateView(CreateView):
+    model = Post
+    template_name = 'post_create.html'
+    form_class = PostForm
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['title'] = 'Create'
+        return context
+
+   
+    def form_valid(self, form):
+        if self.request.user.is_authenticated:
+            form.instance.author = get_author(self.request.user)
+            form.save()
+            return redirect(reverse("post-detail", kwargs={
+                'pk': form.instance.pk,
+                'slug': slugify(form.instance.title)
+            }))
+        else:
+            return redirect("/accounts/login")
+
+
+
+ 
+
+def upload(request):
+    try:
+        file = request.FILES['docf']
+        updatedImgName = str(uuid.uuid4())
+        imgExt = file.name.split('.')[1]
+        img = Image.open(file)
+        img.thumbnail((500, 500), Image.ANTIALIAS)
+        try:
+            img.save(settings.MEDIA_ROOT+"/blog_img/" + updatedImgName+"."+imgExt)
+            print("img save pass")
+        except:
+            print("img.save error")
+        path = "/media/blog_img/" + updatedImgName+"."+imgExt
+
+        return JsonResponse({"imgpath": path}, status=200)
+    except Exception:
+        return HttpResponse("error")
+
+class PostUpdateView(UpdateView):
+    model = Post
+    template_name = 'post_create.html'
+    form_class = PostForm
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['title'] = 'Update'
+        return context
+
+    def form_valid(self, form):
+        form.instance.author = get_author(self.request.user)
+        form.save()
+        return redirect(reverse("post-detail", kwargs={
+            'pk': form.instance.pk,
+            'slug': slugify(form.instance.title)
+        }))
+
+def VoteView(request):
+    iddy = request.POST.get('post_id')
+    if request.method == 'POST':
+        post = get_object_or_404(Post, id= iddy)
+        liked = False
+        if post.votes.filter(id = request.user.id).exists():
+            post.votes.remove(request.user)
+            liked = False
+        else:
+            post.votes.add(request.user)
+            liked = True
+    return JsonResponse({"data": liked}, status=200)
+
+
+class PostDeleteView(DeleteView):
+    model = Post
+    success_url = '/blog'
+    template_name = 'post_confirm_delete.html'
+
+class user_dashboard(View):
+     def get(self,request):
+         count=0
+         com_count=0 
+         user=request.user
+         author=get_author(request.user) 
+         my_post_count=post_count(get_author(request.user))
+         my_published_post=Post.objects.filter(author=author)
+         for x in my_published_post:
+               com_count+= Comment.objects.filter(post=x).count()
+                   
+            
+         for n in Post.objects.filter(author=author):
+            count += n.votes.count()
+
+         context={
+                 'author':author,
+                 'total_votes':count,
+                 'post_count':my_post_count,
+                 'my_published_post':my_published_post,
+                 'total_comments':com_count}
+         
+         return render(request,'account/profile_temp.html',context) 
+
+def post_count(user_id):
+  my_post=Post.objects.filter(author=user_id).count()
+  return my_post
+
+def post_delete(request, id):
+    post = get_object_or_404(Post, id=id)
+    post.delete()
+    return redirect(reverse("post-list"))
+
+def PracticeView(request):
+    message_email = ""
+    if request.method == 'GET':
+        return render(request, "practice.html")
