c4be567f156d68955dd24dc9d81cb4785990131b
Singh <A-SINGHP7@legal.regn.net>
2021-05-17 18:59:29 +0530

    migration is added

--- blog/settings.py
+++ blog/settings.py
@@ -91,7 +91,7 @@ if DEBUG:
     DATABASES = {
         'default': {
             'ENGINE': 'django.db.backends.sqlite3',
-            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+            'NAME': os.path.join(BASE_DIR, 'datascience_mastery.sqlite3'),
         }
     }
 else:
